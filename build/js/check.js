288bbe8262180d3dfb524e004d8e1fec0efcb8b4
Larysa Koziarivska <ryssia@ukr.net>
2016-05-30 12:49:36 +0300

    Внесла правки

--- build/js/check.js
+++ build/js/check.js
@@ -1,5 +1,5 @@
 function getMessage(a, b) {
-	if (typeof a === boolean) {
+	if (typeof a === 'boolean') {
 		if (a) {
 			return ('Переданное GIF-изображение анимировано и содержит ' + b + ' кадров');
 		}
@@ -7,7 +7,7 @@ function getMessage(a, b) {
 			return ('Переданное GIF-изображение не анимировано');
 		}
 	}
-	else if (typeof a === number) {
+	else if (typeof a === 'number') {
 		return ('Переданное SVG-изображение содержит ' + a + ' объектов и ' + b * 4 + ' атрибутов');
 	}
 	else if (Array.isArray(a) && !Array.isArray(b)) {
